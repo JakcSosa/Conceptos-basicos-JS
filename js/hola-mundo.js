21ed8b7340919d62445d80b1340c79a3fbea26ea
JakcSosa <jakcson159@gmail.com>
2023-07-26 16:57:20 -0500

    app hola mundo

---
+++ js/hola-mundo.js
@@ -0,0 +1,6 @@
+// primera de app en javascript
+/**
+ * app hola mundo utiliza una funcion llamada console.log();
+ */
+// muestra el resultado en la consola del navegador
+console.log('Hola mundo');
