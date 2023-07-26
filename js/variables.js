02e5fd1e0b7af97e6e4cadcca166a9ebcf4cc1f8
JakcSosa <jakcson159@gmail.com>
2023-07-26 17:28:11 -0500

    26/07/23

---
+++ js/variables.js
@@ -0,0 +1,18 @@
+/**
+ * para declarar variables var, let y const
+ * tipos de datos: null, undefined, string(texto), boolean(true,false), number, symbol, y object 
+ */
+
+let miEdad=29//tipo de dato 'numero'
+
+const miNombre = 'Jakcson'; //tipo de dato 'string'
+
+const casado = false; //tipo 'booleano'
+
+// miNombre = 'Fausto';
+
+miEdad = 29
+
+console.log('Mi nombre: ' + miNombre);
+console.log('Mi edad: ' + miEdad);
+console.log('Estado Civil: ' + casado);
