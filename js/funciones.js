8da1e6f95f607eae4c7f102f127591e48846b34d
JakcSosa <jakcson159@gmail.com>
2023-07-28 19:04:24 -0500

    17/07/23

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,24 @@
 /**
  * Hay varios tipos de funciones: tradicional, de flecha, anonima y asincronica
  */
+
+// funcion tradicional sin argumentos
+function miNombre(){
+    console.log('Mi nombre es: ' + 'Jakcson');
+    console.log(`Mi nombre es: ${'Marta'}`);
+    console.log('Mi nombre es: ' + 'Tajada');
+}
+
+// llamar la funcion
+miNombre(); 
+
+// funcion con argumentos
+/**
+ * 
+ * @param {String} apellido 
+ */
+function miApellido(apellido){
+    console.log(`Mi apellido: ${apellido}`);
+}
+
+miApellido('Asprilla');
